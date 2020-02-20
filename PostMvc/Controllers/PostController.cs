3977d5f9d8f9f9f0d9fdd76d8722a971ac124036
Varundhar_Gandham <Varundhar_Gandham@EPINHYDW013A>
2020-02-20 13:38:38 +0530

    Add project files.

---
+++ PostMvc/Controllers/PostController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PostMvc.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        private UserEntities db = new UserEntities();
+        public ActionResult Index()
+        {
+            return View(db.userposts.ToList());
+        }
+
+        // GET: Post/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Post/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Post/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                userpost e = new userpost();
+
+
+
+                e.username = collection["username"];
+
+                e.password = collection["password"];
+
+            
+
+                UserEntities userRegistrationEntities = new UserEntities();
+
+                userRegistrationEntities.userposts.Add(e);
+
+                userRegistrationEntities.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Post/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Post/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Post/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Post/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ PostMvc/Controllers/userpostsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PostMvc;
+
+namespace PostMvc.Controllers
+{
+    public class userpostsController : Controller
+    {
+        private UserEntities db = new UserEntities();
+
+        // GET: userposts
+        public ActionResult Index()
+        {
+            return View(db.userposts.ToList());
+        }
+
+        // GET: userposts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            userpost userpost = db.userposts.Find(id);
+            if (userpost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userpost);
+        }
+
+        // GET: userposts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: userposts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "username,password,userid")] userpost userpost)
+        {
+            if (ModelState.IsValid)
+            {
+                db.userposts.Add(userpost);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userpost);
+        }
+
+        // GET: userposts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            userpost userpost = db.userposts.Find(id);
+            if (userpost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userpost);
+        }
+
+        // POST: userposts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "username,password,userid")] userpost userpost)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userpost).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userpost);
+        }
+
+        // GET: userposts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            userpost userpost = db.userposts.Find(id);
+            if (userpost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userpost);
+        }
+
+        // POST: userposts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            userpost userpost = db.userposts.Find(id);
+            db.userposts.Remove(userpost);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
